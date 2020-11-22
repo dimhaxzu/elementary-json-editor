bdc8f2ce31706a269a32a664be69567afc55b584
dimhaxzu <dimhaxzu@gmail.com>
2020-11-22 19:54:23 +0300

    Added CSV support
    Added:
    CSV support
    Copy to clipboard button
    Read-only property to output textarea

--- src/app/data-input/data-input.component.ts
+++ src/app/data-input/data-input.component.ts
@@ -11,6 +11,7 @@ import { JsonService } from '../json.service';
 export class DataInputComponent implements OnInit {
   jsonString: string;
   jsonForm;
+  dataFormat = "json";
   file: File = null;
   constructor(
     private jsonService: JsonService,
@@ -21,17 +22,40 @@ export class DataInputComponent implements OnInit {
   }
   
   onSubmit() {
-    this.jsonService.setJson(this.jsonString);
+    if (this.dataFormat == "json") {
+      this.jsonService.setJson(this.jsonString);
+    } else if (this.dataFormat == "csv") {
+      this.jsonService.setJson(JSON.stringify(this.parseCsv(this.jsonString)));
+    } else {
+      console.log("Format not defined");
+    }
   }
   
   onFileload(uploadedFile: FileList) {
       this.file = uploadedFile[0];
       let reader = new FileReader();
       reader.onload = (e) => {
-        this.jsonService.setJson(reader.result as string);
-        this.jsonString = JSON.stringify(this.jsonService.getJson());
+        this.jsonString = reader.result as string;
       }
       reader.readAsBinaryString(this.file);
       
+  }
+  
+  parseCsv(csvString) {
+    let lines = csvString.split('\n');
+    let keys = lines[0].split(',');
+    let values = lines.splice(1).map(function (eachLine) {
+      return eachLine.split(','); });
+    return values.map(function (rowValues) {
+      let row = {};
+      keys.forEach(function (key, index){
+        row[key] = (index < rowValues.length) ? rowValues[index] : null;
+      });
+      return row;
+    });
+  }
+  
+  changeFormat(e) {
+    this.dataFormat = e.target.value;
   }
 }
--- src/app/data-output/data-output.component.ts
+++ src/app/data-output/data-output.component.ts
@@ -11,15 +11,58 @@ import { JsonService } from '../json.service';
 })
 export class DataOutputComponent implements OnInit {
   jsonString: string;
+  csvString: string;
+  shownString: string;
+  dataFormat = "json";
   constructor(private jsonService: JsonService) { }
 
   ngOnInit(): void {
     this.jsonString = JSON.stringify(this.jsonService.getJson());
+    this.shownString = this.jsonString;
   }
     
   saveToFile() {
+    if (this.dataFormat == "json") {
       let blob = new Blob([this.jsonString], {type:"application/json"});
       let url = window.URL.createObjectURL(blob);
-      FileSaver.saveAs(blob, "jsondata.json")
+      FileSaver.saveAs(blob, "saveddata.json")
+    } else if (this.dataFormat == "csv") {
+      let blob = new Blob([this.csvString],{type:"text/csv"});
+      let url = window.URL.createObjectURL(blob);
+      FileSaver.saveAs(blob, "saveddata.csv")
+    }
+  }
+  
+  parseCsv(data) {
+    let csv = "";
+    let keys = Object.keys(data[0]).join(",");
+    let values = data.map(o => Object.values(o).join(',')).join('\n');
+    csv += keys + "\n" + values;
+    return csv;
+  }
+    
+  changeFormat(e) {
+    this.dataFormat = e.target.value;
+    
+    if (this.dataFormat == "csv"){
+      if (this.csvString) {
+        this.shownString = this.csvString;
+      } else {
+        this.csvString = this.parseCsv(JSON.parse(this.jsonString));
+        this.shownString = this.csvString;
       }
+      
+    } else if (this.dataFormat == "json") {
+      this.shownString = this.jsonString;
+      
+    } else {
+      console.log("Format not defined");
+    }
+  }
+  
+  copyToClipboard(inputElement) {
+      inputElement.select();
+      document.execCommand('copy');
+      inputElement.setSelectionRange(0, 0);
+  }
 }
